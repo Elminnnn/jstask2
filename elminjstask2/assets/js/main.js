b1e1a2e529bc2208035c0c689ebe633f7c3a8181
Elminnnn <114989561+Elminnnn@users.noreply.github.com>
2022-11-03 13:44:10 +0400

    Add files via upload

---
+++ elminjstask2/assets/js/main.js
@@ -0,0 +1,83 @@
+// 1
+// let A = +prompt(`Please enter the value:`);
+// while(isNaN(A) || A==""){
+//     A=+prompt(`Please enter the value:`);
+
+// }
+// for(i=1 ; i<1000 ; i++){
+//     if (i%A==0) {
+//         console.log(i);
+        
+//     }
+// }
+
+// 2
+// let A=+prompt(`Please enter the value:`);
+// while(isNaN(A) || A==""){
+//     A=+prompt(`Please enter the value:`);
+
+// }
+// for(n=1 ; n<A ; n++){
+//     if (Math.pow(n,2)<A ) {
+//         console.log(n);
+        
+//     }
+// }
+
+// 3.
+// let A=+prompt(`Please enter the value:`);
+// while(isNaN(A) || A==""){
+//         A=+prompt(`Please enter the value:`);   
+//     }
+// let d=0;
+// for(i=1; i<A; i++){
+//     if (A%i==0){
+//         d=i;
+
+//     }
+   
+// }
+// console.log(d);
+ 
+
+// 4
+
+// let A=+prompt(`Please enter the value:`);
+// while(isNaN(A) || A==""){
+//     A=+prompt(`Please enter the value:`);
+
+// }
+// let B=+prompt(`Please enter the value:`);
+// while(isNaN(B) || B==""){
+//     B=+prompt(`Please enter the value:`);
+
+// }
+// let sum=0;
+// if (A>B) {
+//     for (i=B; i<A; i++) {
+//         if (i%7==0) {
+//              sum+=i
+            
+//         }
+               
+//     }
+//     console.log(sum);
+    
+// }
+// else if(A<B){
+//     console.log(`B cannot be larger than A `);
+
+// }
+// else{
+//     console.log(`B cannot be equal A` );
+// }
+
+
+// 5
+
+
+
+
+
+
+
